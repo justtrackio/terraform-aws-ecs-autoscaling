50bc0c52f19357c7019047059b4fbb60ad4bd88a
Sebastian Struß <struss@justtrack.io>
2024-05-24 14:23:06 +0200

    feat: allow ignoring min_capacity/max_capacity for aws_appautoscaling_target

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_appautoscaling_target" "default" {
-  count              = module.this.enabled ? 1 : 0
+  count              = module.this.enabled && !var.ignore_changes_min_max_capacity ? 1 : 0
   max_capacity       = var.max_capacity
   min_capacity       = var.min_capacity
   resource_id        = "service/${var.cluster_name}/${var.service_name}"
@@ -8,13 +8,27 @@ resource "aws_appautoscaling_target" "default" {
   service_namespace  = "ecs"
 }
 
+resource "aws_appautoscaling_target" "ignore_changes_min_max_capacity" {
+  count              = module.this.enabled && var.ignore_changes_min_max_capacity ? 1 : 0
+  max_capacity       = var.max_capacity
+  min_capacity       = var.min_capacity
+  resource_id        = "service/${var.cluster_name}/${var.service_name}"
+  role_arn           = "arn:aws:iam::${module.this.aws_account_id}:role/aws-service-role/ecs.application-autoscaling.amazonaws.com/AWSServiceRoleForApplicationAutoScaling_ECSService"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+
+  lifecycle {
+    ignore_changes = [min_capacity, max_capacity]
+  }
+}
+
 resource "aws_appautoscaling_policy" "default" {
   count              = module.this.enabled && length(var.schedule) == 0 ? 1 : 0
   name               = module.this.id
   policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.default[count.index].resource_id
-  scalable_dimension = aws_appautoscaling_target.default[count.index].scalable_dimension
-  service_namespace  = aws_appautoscaling_target.default[count.index].service_namespace
+  resource_id        = try(aws_appautoscaling_target.default[0], aws_appautoscaling_target.ignore_changes_min_max_capacity[0]).resource_id
+  scalable_dimension = try(aws_appautoscaling_target.default[0], aws_appautoscaling_target.ignore_changes_min_max_capacity[0]).scalable_dimension
+  service_namespace  = try(aws_appautoscaling_target.default[0], aws_appautoscaling_target.ignore_changes_min_max_capacity[0]).service_namespace
 
   dynamic "target_tracking_scaling_policy_configuration" {
     for_each = var.target_tracking
@@ -47,14 +61,14 @@ resource "aws_appautoscaling_policy" "default" {
 resource "aws_appautoscaling_scheduled_action" "schedule" {
   count              = module.this.enabled && length(var.schedule) > 0 ? length(var.schedule) : 0
   name               = "${module.this.id}-${count.index}"
-  resource_id        = aws_appautoscaling_target.default[0].resource_id
-  scalable_dimension = aws_appautoscaling_target.default[0].scalable_dimension
-  service_namespace  = aws_appautoscaling_target.default[0].service_namespace
+  resource_id        = try(aws_appautoscaling_target.default[0], aws_appautoscaling_target.ignore_changes_min_max_capacity[0]).resource_id
+  scalable_dimension = try(aws_appautoscaling_target.default[0], aws_appautoscaling_target.ignore_changes_min_max_capacity[0]).scalable_dimension
+  service_namespace  = try(aws_appautoscaling_target.default[0], aws_appautoscaling_target.ignore_changes_min_max_capacity[0]).service_namespace
 
-  schedule = lookup(var.schedule[count.index], "schedule")
+  schedule = var.schedule[count.index]["schedule"]
 
   scalable_target_action {
-    min_capacity = lookup(var.schedule[count.index], "min_capacity")
-    max_capacity = lookup(var.schedule[count.index], "max_capacity")
+    min_capacity = var.schedule[count.index]["min_capacity"]
+    max_capacity = var.schedule[count.index]["max_capacity"]
   }
 }
